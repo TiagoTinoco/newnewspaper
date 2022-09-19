68626752a38902de60adf9b8a54b1b1dbf9c2dcf
Tiago Tinoco <tiagotinocomartinsdossantos@gmail.com>
2022-09-19 16:31:54 -0300

    Create estructure site

---
+++ java.js
@@ -0,0 +1,7 @@
+document.getElementById("open").onclick=()=>{
+    document.getElementById("menu").style.right="0px";
+}
+
+document.getElementById("close").onclick=()=>{
+    document.getElementById("menu").style.right="-200px";
+}
